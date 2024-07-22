a79c64571bd05d15e47655918be80c573c1c4927
DexSAFE <dexsafe.github@gmail.com>
2024-07-22 16:05:53 +0300

    init

--- Package.swift
+++ Package.swift
@@ -14,10 +14,10 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/attaswift/BigInt.git", .upToNextMajor(from: "5.0.0")),
-        .package(url: "https://github.com/horizontalsystems/EvmKit.Swift.git", .upToNextMajor(from: "2.0.0")),
-        .package(url: "https://github.com/horizontalsystems/Eip20Kit.Swift.git", .upToNextMajor(from: "2.0.0")),
-        .package(url: "https://github.com/horizontalsystems/HsCryptoKit.Swift.git", .upToNextMajor(from: "1.0.0")),
-        .package(url: "https://github.com/horizontalsystems/HsExtensions.Swift.git", .upToNextMajor(from: "1.0.0")),
+        .package(url: "https://github.com/DexSAFE/EvmKit.Swift.git", .upToNextMajor(from: "2.0.0")),
+        .package(url: "https://github.com/DexSAFE/Eip20Kit.Swift.git", .upToNextMajor(from: "2.0.0")),
+        .package(url: "https://github.com/DexSAFE/HsCryptoKit.Swift.git", .upToNextMajor(from: "1.0.0")),
+        .package(url: "https://github.com/DexSAFE/HsExtensions.Swift.git", .upToNextMajor(from: "1.0.0")),
     ],
     targets: [
         .target(
